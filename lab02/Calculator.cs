1380b7ff37e6fe0e66c54c1684f49f6bc7bedabf
Irin Krapookthong (NTL) <CF303520@tidlor.com>
2021-07-03 11:34:39 +0700

    init

---
+++ lab02/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab02
+{
+    public class Calculator
+    {
+        public double number = 0.0; //properties
+
+        public Calculator(double number)//constructor
+        {
+            this.number = number;//Dependency
+        }
+        public string Calcutor()
+        {
+            var result = "";
+            if (number >= 50)
+            {
+                result = "pass";
+            }
+            else  
+            {
+                result = "fail";
+            }
+            return result;
+        }public double Display()
+           
+        {
+            return number;
+        }
+    }
+}
---
+++ lab02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Calculator");
+
+            Console.Write("number=");
+
+            var number = Double.Parse(Console.ReadLine()) ;
+            var Calc = new Calculator(number);
+
+          var result =  Calc.Calcutor();
+            Console.WriteLine(" :your result: "+result);
+
+            var inputnumber = Calc.Display();
+            Console.WriteLine(":your number:"+inputnumber);
+            Console.ReadLine();
+            
+        }
+
+       
+    }
+}
